ad88cafb551ea04fa6f5b34965b4ec4912bde6b4
Jfkmdd <federico.deduro@gmail.com>
2021-12-22 16:08:02 +0100

    Aggiunta scatter plot
    Aggiunti scatter plot per
    X = regressore
    Y = PM10

--- G08_Berlino_AssReg_calcoli.m
+++ G08_Berlino_AssReg_calcoli.m
@@ -29,3 +29,65 @@ fitlm(t(:, [vn(end-8:end); vn(end-9)]))
 corrcoef(t{:, vn(end-8:end)})
 
 
+% GRAFICI PM10 %
+y = t.PM10;
+
+% Umidità - PM10
+x = t.Umidita_relativa;
+scatter(x,y,'filled')
+title('Umidità e PM10')
+xlabel('Umidità: %')
+ylabel('PM10: mug/m^3')
+lsline
+M = corrcoef(x,y);
+indice_di_correlazione = M(1, 2)
+
+% Temperatura - PM10
+x = t.Temperatura;
+scatter(x,y,'filled')
+title('Temperatura e PM10')
+xlabel('Temperatura: °')
+ylabel('PM10: mug/m^3')
+lsline
+M = corrcoef(x,y);
+indice_di_correlazione = M(1, 2)
+
+% Pioggia - PM10
+x = t.Pioggia_cum;
+scatter(x,y,'filled')
+title('Pioggia e PM10')
+xlabel('Pioggia: nm')
+ylabel('PM10: mug/m^3')
+lsline
+M = corrcoef(x,y);
+indice_di_correlazione = M(1, 2)
+
+% Ozono
+x = t.O3;
+scatter(x,y,'filled')
+title('O3 e PM10')
+xlabel('O3: mug/m^3')
+ylabel('PM10: mug/m^3')
+lsline
+M = corrcoef(x,y);
+indice_di_correlazione = M(1, 2)
+
+% Ossidi Azoto
+x = t.NOx;
+scatter(x,y,'filled')
+title('NOx e PM10')
+xlabel('NOx: mug/m^3')
+ylabel('PM10: mug/m^3')
+lsline
+M = corrcoef(x,y);
+indice_di_correlazione = M(1, 2)
+
+% Biossido di azoto
+x = t.NO2
+scatter(x,y,'filled')
+title('NO2 e PM10')
+xlabel('NO2: mug/m^3')
+ylabel('PM10: mug/m^3')
+lsline
+M = corrcoef(x,y);
+indice_di_correlazione = M(1, 2)
